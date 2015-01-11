69a7f4885c1e192620548d95fff4ea2039bf0662
Vanya Sergeev <vsergeev@gmail.com>
2015-01-10 18:09:20 -0800

    update test config with textstats postprocessor block

--- configs/test_config.lua
+++ configs/test_config.lua
@@ -1,5 +1,7 @@
 configuration = {
     timestep = 2.0,
+    dbfile = nil,
+    logfile = nil,
     inputs = {
         bar = {
             driver = "dummy",
@@ -7,7 +9,15 @@ configuration = {
             values = {1234, 4567, 100, 200, 300}
         },
     },
-    controllers = {},
-    outputs = {},
-    postprocessors = {},
+    controllers = {
+    },
+    outputs = {
+    },
+    postprocessors = {
+        textstats = {
+            driver = "textstats",
+            variables = { {name = "foo", units = "°C"} },
+            file = nil,
+        },
+    },
 }
