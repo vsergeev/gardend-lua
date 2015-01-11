03d1f115a157f76f1b6582f3105953dc4f425326
Vanya Sergeev <vsergeev@gmail.com>
2015-01-10 20:36:37 -0800

    add configuration checks to dummy input block

--- inputs/dummy.lua
+++ inputs/dummy.lua
@@ -4,14 +4,22 @@ setmetatable(dummy, {__call = function(self, ...) return self.new(...) end})
 
 function dummy.new(configuration)
     local self = setmetatable({}, dummy)
-    self.var = configuration.variables[1]
+
+    if #configuration.variables ~= 1 then
+        error("invalid number of state variables in configuration. expected 1")
+    elseif configuration.values == nil then
+        error("missing values array in configuration.")
+    end
+
+    self.variable = configuration.variables[1]
     self.values = configuration.values
     self.i = 0
+
     return self
 end
 
 function dummy:process(state)
-    state[self.var] = self.values[self.i+1]
+    state[self.variable] = self.values[self.i+1]
     self.i = (self.i + 1) % #self.values
 end
 
